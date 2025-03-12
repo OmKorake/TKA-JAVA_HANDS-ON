47e4113cfc1d058488e83ed5b4e8c1718f60d9e4
OmKorake <ompkorke2004@gmail.com>
2025-03-12 22:10:53 +0530

    Add files via upload

---
+++ controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import com.service.StudentService;
+
+public class StudentController {
+
+	public static void main(String[] args) {
+
+		StudentService ss = new StudentService();
+//		ss.insertData();
+//		ss.updateData();
+//		ss.deleteData();
+//		ss.GetPartiData();
+		ss.GetAllData();
+		
+
+
+		
+	}
+
+}
